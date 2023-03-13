ff7621b28a1b5fd0992a48d7ea12f7ce674bff77
cloudwoon <cloud.wooo0n@gmail.com>
2023-03-13 10:26:49 +0900

    02-1. 배열이란?

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,64 @@ import java.util.*;
 
 public class Main {
 
-    public String solution(String s) {
-        String answer = "";
-        //char answer[] = new char[s.length()];
-        char c = ' '; //이전 알파벳
-        int count = 1;
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+        System.out.println("a[" + idx1 + "]과 a[" + idx2 + "]을 교환합니다.");
+        System.out.println(Arrays.toString(a));
+    }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (c == s.charAt(i)) {
-                //이전 알파벳과 현재 알파벳이 같을 때
-                count++; //몇 번 연속하는지 카운트 올리기
-            } else {
-                //현재 알파벳과 이전 알파벳이 다를 때
-                if(count == 1){
-                    answer = answer + s.charAt(i); //알파벳 넣어주기
-                }else {
-                    answer = answer + count + s.charAt(i);
-                    count = 1;
-                }
+    static int sumOf(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++)
+            sum = sum + a[i];
+        return sum;
+    }
+
+    static void reverse(int[] a) {
+        for (int i = 0; i < a.length / 2; i++)
+            swap(a, i, a.length - i - 1);
+
+        System.out.println("역순 정렬을 마쳤습니다.");
+    }
+
+    static void copy(int[] a, int[] b) {
+        if(b.length>=a.length){
+            for(int i = 0 ; i < a.length; i++){
+                a[i] = b[i];
             }
-             c = s.charAt(i);
+        }else {
+            for (int i = 0; i <b.length; i++){
+                a[i] = b[i];
+            }
         }
-        if(count!=1) answer = answer + count;
 
-        return answer;
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        System.out.print(T.solution(str));
+
+        System.out.print("요솟수:");
+        int num = kb.nextInt();
+
+        int[] x = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("x[" + i + "]: ");
+            x[i] = kb.nextInt();
+        }
+
+       /* int sum = sumOf(x);
+        System.out.println(sum);*/
+
+        int[] a = new int[5];
+        copy(a,x);
+        System.out.println(Arrays.toString(a));
+        //reverse(x);
+
+        /*System.out.println("요소를 역순으로 정렬했음.");
+        System.out.println("x ="+Arrays.toString(x));*/
+
     }
 }
