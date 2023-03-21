ced5ef5c15b2754e36f518f0a60f01d51777a994
cloudwoon <cloud.wooo0n@gmail.com>
2023-03-21 15:21:53 +0900

    2-4. 피보나치 수열

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
 import java.util.*;
+
 class Main {
-    public String solution(int n, int[] a, int[] b){
-        String answer="";
-        for(int i=0; i<n; i++){
-            if(a[i]==b[i]) answer+="D";
-            else if(a[i]==1 && b[i]==3) answer+="A";
-            else if(a[i]==2 && b[i]==1) answer+="A";
-            else if(a[i]==3 && b[i]==2) answer+="A";
-            else answer+="B";
+    public int[] solution(int n) {
+        int[] answer = new int[n + 1];
+        answer[0] = 0;
+        answer[1] = 1;
+        System.out.print(answer[1]+" ");
+        for (int i = 2; i < n + 1; i++) {
+            answer[i] = answer[i - 2] + answer[i-1];
+            System.out.print(answer[i] + " ");
         }
         return answer;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int[] a=new int[n];
-        int[] b=new int[n];
-        for(int i=0; i<n; i++){
-            a[i]=kb.nextInt();
-        }
-        for(int i=0; i<n; i++){
-            b[i]=kb.nextInt();
-        }
-        for(char x : T.solution(n, a, b).toCharArray()) System.out.println(x);
+        int n = kb.nextInt();
+        T.solution(n);
     }
 }
