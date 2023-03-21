a226a9b8bc9ea7b6b7178200e1570424fa4f0a4d
cloudwoon <cloud.wooo0n@gmail.com>
2023-03-21 14:42:57 +0900

    2-3.가위 바위 보

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,28 @@
 import java.util.*;
-
-public class Main {
-
-    static void swap(int[] a, int idx1, int idx2) {
-        int t = a[idx1];
-        a[idx1] = a[idx2];
-        a[idx2] = t;
-        System.out.println("a[" + idx1 + "]과 a[" + idx2 + "]을 교환합니다.");
-        System.out.println(Arrays.toString(a));
-    }
-
-    static int sumOf(int[] a) {
-        int sum = 0;
-        for (int i = 0; i < a.length; i++)
-            sum = sum + a[i];
-        return sum;
-    }
-
-    static void reverse(int[] a) {
-        for (int i = 0; i < a.length / 2; i++)
-            swap(a, i, a.length - i - 1);
-
-        System.out.println("역순 정렬을 마쳤습니다.");
-    }
-
-    static void copy(int[] a, int[] b) {
-        if(b.length>=a.length){
-            for(int i = 0 ; i < a.length; i++){
-                a[i] = b[i];
-            }
-        }else {
-            for (int i = 0; i <b.length; i++){
-                a[i] = b[i];
-            }
+class Main {
+    public String solution(int n, int[] a, int[] b){
+        String answer="";
+        for(int i=0; i<n; i++){
+            if(a[i]==b[i]) answer+="D";
+            else if(a[i]==1 && b[i]==3) answer+="A";
+            else if(a[i]==2 && b[i]==1) answer+="A";
+            else if(a[i]==3 && b[i]==2) answer+="A";
+            else answer+="B";
         }
-
+        return answer;
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Main T = new Main();
         Scanner kb = new Scanner(System.in);
-
-        System.out.print("요솟수:");
-        int num = kb.nextInt();
-
-        int[] x = new int[num];
-
-        for (int i = 0; i < num; i++) {
-            System.out.print("x[" + i + "]: ");
-            x[i] = kb.nextInt();
+        int n=kb.nextInt();
+        int[] a=new int[n];
+        int[] b=new int[n];
+        for(int i=0; i<n; i++){
+            a[i]=kb.nextInt();
         }
-
-       /* int sum = sumOf(x);
-        System.out.println(sum);*/
-
-        int[] a = new int[5];
-        copy(a,x);
-        System.out.println(Arrays.toString(a));
-        //reverse(x);
-
-        /*System.out.println("요소를 역순으로 정렬했음.");
-        System.out.println("x ="+Arrays.toString(x));*/
-
+        for(int i=0; i<n; i++){
+            b[i]=kb.nextInt();
+        }
+        for(char x : T.solution(n, a, b).toCharArray()) System.out.println(x);
     }
 }
