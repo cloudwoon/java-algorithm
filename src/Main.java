18d2bc305e135752616617dfcbe7f2b292c8112d
cloudwoon <cloud.wooo0n@gmail.com>
2023-04-04 16:30:26 +0900

    2-11. 임시반장 정하기 (오답코드)

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,42 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+    public int solution(int num, int[][] classNum) {
+        int answer = 0;
+        int max = 0;
+        for (int i = 0; i < num; i++) {
+            int count = 0;
+            for (int k = 0; k < 5; k++) {
+                for (int j = 1; j < num; j++) {
+                    if (classNum[i][k] == classNum[j][k]) count++;
+                }
+                if (count > max) max = count;
+                answer = i;
+            }
+        }
+        return answer;
+    }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine()); // 카드의 개수
-        int[] cards = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[][] classNum = new int[N][5];
 
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int k = 0; k < 5; k++) {
+                classNum[i][k] = Integer.parseInt(st.nextToken());
+            }
         }
-
-        Arrays.sort(cards); // 이분탐색할 배열은 정렬되어 있어야 함.
-        int M = Integer.parseInt(br.readLine()); // 구별할 수의 개수
-
-        StringBuilder sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int temp = Integer.parseInt(st.nextToken());
-            sb.append(binarySearch(cards, N, temp) + " ");
-        }
-
-        bw.write(sb.toString() + "\n");
+        bw.write(String.valueOf(T.solution(N, classNum)));
         bw.flush();
         bw.close();
-        br.close();
     }
 
-    public static int binarySearch(int[] cards, int N, int search) {
-        int first = 0;
-        int last = N - 1;
-        int mid = 0;
-
-        while (first <= last) {
-            mid = (first + last) / 2; // 중간 인덱스
-
-            if (cards[mid] == search) { // 중간값과 찾으려는 수가 같은 경우
-                return 1;
-            }
-
-            if (cards[mid] < search) { // 중간값이 찾으려는 수보다 작으면, 그 이하로는 볼 필요 없음.
-                first = mid + 1;
-            } else { // 중간값이 찾으려는 수보다 크면, 그 이상으로는 볼 필요 없음.
-                last = mid - 1;
-            }
-        }
-
-        return 0;
-    }
-
+    //오답
 }
