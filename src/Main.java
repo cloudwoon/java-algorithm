d1771a923b38240d2fe127586e65d49e653125b9
cloudwoon <cloud.wooo0n@gmail.com>
2023-04-07 15:36:58 +0900

    3-1. 두 배열 합치기

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,15 @@
 import java.io.*;
-import java.util.StringTokenizer;
-
+import java.util.*;
 class Main {
-    public int solution(int num, int[][] classNum) {
-        int answer = 0;
-        int max = 0;
-        for (int i = 1; i <= num; i++) {
-            int count = 0;
-            for (int k = 1; k <= num; k++) {
-                for (int j = 1; j <= 5; j++) {
-                    if (classNum[i][j] == classNum[k][j]) {
-                        count++;
-                        break;
-                    }
-                }
-            }
-            if (count > max) {
-                max = count;
-                answer = i;
-            }
+    public void solution(int N, int M, int[] Nnum, int[] Mnum) {
+        int[] answer = new int[N + M];
+        System.arraycopy(Nnum, 0, answer, 0, Nnum.length);
+        System.arraycopy(Mnum, 0, answer, Nnum.length, Mnum.length);
+        Arrays.sort(answer);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i] + " ");
         }
-        return answer;
     }
-
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,17 +17,17 @@ class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[][] classNum = new int[N + 1][6];
-
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int k = 1; k <= 5; k++) {
-                classNum[i][k] = Integer.parseInt(st.nextToken());
-            }
+        int[] Nnum = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            Nnum[i] = Integer.parseInt(st.nextToken());
         }
-        bw.write(String.valueOf(T.solution(N, classNum)));
-        bw.flush();
-        bw.close();
+        int M = Integer.parseInt(br.readLine());
+        int[] Mnum = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            Mnum[i] = Integer.parseInt(st.nextToken());
+        }
+        T.solution(N, M, Nnum, Mnum);
     }
-    //수정본, 정답
 }
