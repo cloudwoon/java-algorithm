345ffb90db91bf65b7d22a53a69367a6ed6389cb
cloudwoon <cloud.wooo0n@gmail.com>
2023-04-09 17:00:44 +0900

    백준 2003번 수들의 합2 오답

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,25 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int p1=0,p2=0;
+    public int solution(int n, int m, int[] num) {
+        int answer = 0;
+        int s = 0;
+        int e = 0;
+        int sum = 0;
+        Arrays.sort(num);
+        //n = 4 , m = 2
+        while(true) {
+            if(sum >= m) {
+                sum -= num[s++];
+            }
+            else if( e == n) break;
+            else if(sum < m) {
+                sum += num[e++];
+            }
 
-        while (p1<n && p2<m){
-            if (a[p1] == b[p2]) {
-                answer.add(a[p1++]);
-                p2++;
-            }else if (a[p1] < b[p2]) p1++;
-            else p2++;
+            if(sum == m) {
+                answer++;
+            }
         }
         return answer;
     }
@@ -21,26 +28,19 @@ class Main {
     public static void main(String[] args) throws IOException, IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] Nnum = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            Nnum[i] = Integer.parseInt(st.nextToken());
-        }
-        int M = Integer.parseInt(br.readLine());
-        int[] Mnum = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            Mnum[i] = Integer.parseInt(st.nextToken());
+            num[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int x : T.solution(N, M, Nnum, Mnum)) {
-            bw.write(x+" ");
-        }
-        bw.flush();
-        bw.close();
+        System.out.print(T.solution(N,M,num));
     }
+    //오답 
 }
